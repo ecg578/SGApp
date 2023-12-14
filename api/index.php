d276714b861bd9ee76894f7da06be2c2b9351ccf
ecg578 <ecg578@inlumine.ual.es>
2023-12-14 21:27:26 +0100

    cambios conexion

--- api/index.php
+++ api/index.php
@@ -28,7 +28,7 @@
       <tbody>
         <?php
 
-        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "SG");
+        $conexion = mysqli_connect(getenv(34.134.145.147), getenv(sg), getenv(pedroloco98), "SG");
 
         $cadenaSQL = "select * from s_customer";
         $resultado = mysqli_query($conexion, $cadenaSQL);
